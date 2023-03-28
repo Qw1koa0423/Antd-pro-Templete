b2434708182e44deff28ddb33d70a9d24057757e
刘浩奇 <liuhaoqi@yaozai.net>
2023-03-28 18:04:53 +0800

    ✨ feat(login): 登录

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,15 @@
 /*
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-03-22 11:39:51
- * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-03-22 13:13:14
- * @FilePath: \Templete\src\pages\User\Login\index.tsx
+ * @LastEditors: ������ liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-03-26 13:16:56
+ * @FilePath: \Templete\src\pages\Login\index.tsx
  * @Description: 登录
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
 
-import { login } from '@/services/user/api';
+import { login } from '@/services/account/api';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { LoginForm, ProFormText } from '@ant-design/pro-components';
 import { useEmotionCss } from '@ant-design/use-emotion-css';
@@ -17,7 +17,8 @@ import { Helmet, history, useModel } from '@umijs/max';
 import { message } from 'antd';
 import React from 'react';
 import { flushSync } from 'react-dom';
-import Settings from '../../../../config/defaultSettings';
+import Settings from '../../../config/defaultSettings';
+
 const Login: React.FC = () => {
   const { setInitialState } = useModel('@@initialState');
   const containerClassName = useEmotionCss(() => {
@@ -33,25 +34,17 @@ const Login: React.FC = () => {
       backgroundSize: '100% 100%',
     };
   });
-  const handleSubmit = async (values: User.LoginParams) => {
+  const handleSubmit = async (values: Account.LoginParams) => {
     try {
       // 登录
-      const userInfo = await login({
-        ...values,
-      });
-      window.localStorage.setItem(
-        'userInfo',
-        JSON.stringify({ ...userInfo, account: values.account }),
-      );
-      window.sessionStorage.setItem(
-        'userInfo',
-        JSON.stringify({ ...userInfo, account: values.account }),
-      );
+      const userInfo = await login(values);
+      window.localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      window.sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
       if (userInfo) {
         flushSync(() => {
           setInitialState((s: any) => ({
             ...s,
-            currentUser: { ...userInfo, account: values.account },
+            currentUser: userInfo,
           }));
         });
         message.success('登录成功！');
@@ -59,7 +52,7 @@ const Login: React.FC = () => {
         history.push('/');
       }
     } catch (error: any) {
-      if (error.errorMessage.length > 0) {
+      if (error?.errorMessage.length > 0) {
         message.error(error.errorMessage);
       } else {
         message.error('登录失败，请重试！');
@@ -89,7 +82,7 @@ const Login: React.FC = () => {
           title="后台通用模板"
           subTitle="后台通用模板"
           onFinish={async (values) => {
-            await handleSubmit(values as User.LoginParams);
+            await handleSubmit(values as Account.LoginParams);
           }}
         >
           <ProFormText
